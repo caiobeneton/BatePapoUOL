37695e60c6013abdfdd5a6bc544e240f1628170d
Caio Beneton <caio.beneton@gmail.com>
2022-08-26 22:50:49 -0300

    feat: enviar mensagem

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ function entrarSala() {
     promisse.then(nomeAprovado);
     promisse.catch(outroNome);
 };
-entrarSala()
+entrarSala();
+pegarDados();
 function nomeAprovado(){
     setInterval(manterConexao, 5000);
 };
@@ -15,14 +16,13 @@ function nomeAprovado(){
 function outroNome(erro){
     const statusCode = erro.response.status;
     if (statusCode === 400) {
-        alert('Esse nome já está sendo utilizado, escolha outro')
-        entrarSala()
+        alert('Esse nome já está sendo utilizado, escolha outro');
+        entrarSala();
     }
 }
 
 function manterConexao(){
     axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {name: nome});
-    console.log('conexão ok')
 }
 
 /*-------------------------------------------------------------------------------------------*/
@@ -31,8 +31,8 @@ function pegarDados() {
     const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promessa.then(processarDados);
 }
-setInterval(pegarDados, 3000)
-//pegarDados()
+setInterval(pegarDados, 3000);
+
 function processarDados(resposta) {
     chat = resposta.data;
     renderizarChat();
@@ -55,6 +55,12 @@ function renderizarChat() {
                     <span class="hora">(${chat[i].time}) </span> <span class="user">${chat[i].from}</span> para <span class="user">${chat[i].to}</span>: ${chat[i].text}
                 </p>
             </li>`;
+        } else if (chat[i].type === 'private_message' && chat[i].to === nome) {
+            ul.innerHTML += `<li class="mensagem pvt">
+                <p>
+                    <span class="hora">(${chat[i].time}) </span> <span class="user">${chat[i].from}</span> para <span class="user">${chat[i].to}</span>: ${chat[i].text}
+                </p>
+            </li>`;
         }
 
         const msgAtt = document.querySelectorAll('.mensagem');
@@ -62,4 +68,24 @@ function renderizarChat() {
         last.scrollIntoView();
     }
 
+}
+/*--------------------------------------------------------------------------------------------------------------------- */
+
+function enviarMensagem() {
+    const mensagem = document.querySelector('#texto-msg');
+    const novaMensagem = {
+        from: nome,
+        to: "Todos",
+        text: mensagem.value,
+        type: "message"
+    };
+
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', novaMensagem);
+    promessa.catch(naoEnviado);
+
+    mensagem.value = '';
+
+}
+function naoEnviado(){
+    location.reload();
 }
